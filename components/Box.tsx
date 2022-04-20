be80949fa6cf9a23982bdd168ee4c643fc9e709f
austiniqer <63011075@kmitl.ac.th>
2022-04-21 02:01:22 +0700

    add some more

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,11 +1,15 @@
 import React, { FC } from "react";
 import styles from "../styles/Box.module.scss";
 import Image from "next/image";
+import { BsGithub } from "react-icons/bs";
+import Link from "next/link";
+import { CRED } from "../pages/aboutus";
 
-const Box: FC<{ img: string; name: string; id: number }> = ({
+const Box: FC<CRED> = ({
   img,
   name,
   id,
+  github
 }) => {
   return (
     <div className={styles.container}>
@@ -20,6 +24,11 @@ const Box: FC<{ img: string; name: string; id: number }> = ({
       <div className={styles.wrapper}>
         <h3 className={styles.id}>{id}</h3>
         <p className={styles.name}>{name}</p>
+        <Link href={github} passHref>
+          <a target={"_blank"}>
+            <BsGithub size={"32px"} className={styles.github} />
+          </a>
+        </Link>
       </div>
     </div>
   );
--- pages/aboutus.tsx
+++ pages/aboutus.tsx
@@ -3,31 +3,43 @@ import React from "react";
 import Box from "../components/Box";
 import styles from "../styles/Aboutus.module.scss";
 
-const members = [
+export type CRED = {
+  img: string;
+  name: string;
+  id: number;
+  github: string;
+};
+
+const members: CRED[] = [
   {
     img: "/members/kim.png",
     name: "Abdulhakim Mamu",
     id: 63011414,
+    github: "https://github.com/hakimparoo",
   },
   {
     img: "/members/king.png",
     name: "Abdulhakim Maha",
     id: 63011075,
+    github: "https://github.com/Abdulhakim-Maha",
   },
   {
     img: "/members/newyear.png",
     name: "Watanai Maythamaluang ",
     id: 63010846,
+    github: "https://github.com/Watanai1245",
   },
   {
     img: "/members/fome.png",
     name: "Phurich Chanprasit",
     id: 63010767,
+    github: "https://github.com/Phurich63010767",
   },
   {
     img: "/members/pol.png",
     name: "Warapob Keatkongsang",
     id: 63010841,
+    github: "https://github.com/Warapob",
   },
 ];
 
@@ -40,7 +52,13 @@ const Aboutus = () => {
       <div className={styles.container}>
         <div className={styles.wrapper}>
           {members.map((mb) => (
-            <Box key={mb.id} id={mb.id} img={mb.img} name={mb.name} />
+            <Box
+              key={mb.id}
+              id={mb.id}
+              img={mb.img}
+              name={mb.name}
+              github={mb.github}
+            />
           ))}
         </div>
       </div>
