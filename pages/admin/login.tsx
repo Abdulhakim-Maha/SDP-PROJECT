1888f02181229ed659a552d555a841b030eb54b9
austiniqer <63011075@kmitl.ac.th>
2022-04-26 22:18:58 +0700

    .

--- pages/admin/login.tsx
+++ pages/admin/login.tsx
@@ -11,7 +11,7 @@ const Login = () => {
 
   const handleClick = async () => {
     try {
-      await axios.post("https://sdp-project-rm4nichqi-abdulhakim-maha.vercel.app/api/login", {
+      await axios.post("https://sdp-project.vercel.app/api/login", {
         username,
         password,
       });
