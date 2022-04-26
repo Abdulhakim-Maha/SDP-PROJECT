17bb4ce2b60ee991565df649bf3a8112a30c926b
austiniqer <63011075@kmitl.ac.th>
2022-04-26 23:19:38 +0700

    s

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/orders",
+        destination: "https://sdp-project.vercel.app/api/orders",
+      },
+    ];
+  },
   images: {
-    domains: ["res.cloudinary.com","sdp-project.vercel.app","sdp-project-git-main-abdulhakim-maha.vercel.app","sdp-project-abdulhakim-maha.vercel.app"],
+    domains: [
+      "res.cloudinary.com",
+      "sdp-project.vercel.app",
+      "sdp-project-git-main-abdulhakim-maha.vercel.app",
+      "sdp-project-abdulhakim-maha.vercel.app",
+    ],
   },
 };
--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,8 +1,16 @@
 import dbConnect from "../../../util/dbConnet";
 import Order from "../../../models/Order";
 import { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   await dbConnect();
   const { method } = req;
   if (method === "GET") {
